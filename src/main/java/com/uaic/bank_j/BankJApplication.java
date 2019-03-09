230750080654733de2f934ad9d44e01473d37038
unknown <dragosdulan@yahoo.com>
2019-03-09 14:33:11 +0200

    Primu commit

---
+++ src/main/java/com/uaic/bank_j/BankJApplication.java
@@ -0,0 +1,13 @@
+package com.uaic.bank_j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uaic/bank_j/BankJApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uaic.bank_j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankJApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
